ad2bdc371660506a4b2e14f8d37c8030df4c9dcd
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-11 14:06:42 +0300

    added cloudinary

--- awwards/settings.py
+++ awwards/settings.py
@@ -9,8 +9,12 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
+import cloudinary
+import cloudinary.uploader
+import cloudinary.api
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,7 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'awards'
+    'awards.apps.AwardsConfig',
+    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -81,6 +86,11 @@ DATABASES = {
     }
 }
 
+cloudinary.config( 
+  cloud_name = "dgtjsiwow", 
+  api_key = "346831399935182", 
+  api_secret = "ofyOVu1Z9VbnIUyx9BJ1oCHWd1w" 
+)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
