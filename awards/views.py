c125ad7213936ea0939b884cec476b0ecbd7e71b
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-11 20:04:13 +0300

    created superuser

--- awards/views.py
+++ awards/views.py
@@ -1,8 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from .models import Project, Profile, Review
+from .forms import ProfileForm, ProjectForm, RateForm
+
 
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def index(request):
+    # current_user = request.user
+    # try:
+    #     if not request.user.is_authenticated:
+    #         return redirect('accounts/login')
+    #      current_user = request.user
+    #      profile = Profile.objects.get(user=current_user)
+
+    # except 
+
+
     return render(request, 'index.html',)
