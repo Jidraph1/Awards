656d1dc77ad5c603575bd7ac091dfb925e27d3aa
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-11 14:30:44 +0300

    review model with rating choices

--- awards/models.py
+++ awards/models.py
@@ -18,7 +18,44 @@ class Profile(models.Model):
         return self.user 
     
 
-class Project(models.Model):
-    user = models.ForeignKey(User, on_delete= models.CASCADE, related_name= 'profile-user')
+class Project(models.Model):   
+    user = models.ForeignKey(User,on_delete = models.CASCADE, related_name='project_user')
     title = models.CharField(max_length=100)
+    image = CloudinaryField('image') 
+    description = HTMLField()
+    link = models.CharField(max_length=250)
+    posted_at = models.DateTimeField(auto_now_add=True)
+    country = models.CharField(max_length=100, default="")
+  
 
+    # @classmethod
+    # def search_project(cls,search_term):
+    #     projects = cls.objects.filter(Q(user__username=search_term) | Q(title__icontains=search_term))
+    #     return projects  
+      
+    
+    def __str__(self):
+        return self.title 
+
+class Review(models.Models):
+
+    RATE_CHOICES = [
+	(1,'1 - Unsatisfactory'),
+	(2,'2 - Pathetic'),
+	(3,'3 - Very bad'),
+	(4,'4 - Bad'),
+	(5,'5 - Average'),
+	(6,'6 - Okay'),
+	(7,'7 - Good'),
+	(8,'8 - Very Good'),
+	(9,'9 - Excellent'),
+	(10, '10 - Perfect'), 
+]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    project = models.ForeignKey(Project, on_delete = models.CASCADE)
+    design = models.PositiveSmallIntegerField(choices=RATE_CHOICES, default=5) 
+    usability =models.PositiveSmallIntegerField(choices=RATE_CHOICES, default=5)  
+    content = models.PositiveSmallIntegerField(choices=RATE_CHOICES, default=5)
+    comment = models.CharField(max_length=250, blank=True,default='')  
+    overall = models.IntegerField(blank=True,default=0) 
