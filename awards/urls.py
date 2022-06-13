54036b7db76b7bd9c3739df47bc81d5ce518696b
Jidraph! <jidraph.kimachia@student.moringaschool.com>
2022-06-13 19:17:03 +0300

    submit and search functionality

--- awards/urls.py
+++ awards/urls.py
@@ -7,4 +7,7 @@ urlpatterns =[
     path('', views.index, name = 'index'),
     path('update-profile',views.update_profile, name='update_profile'),
     path('profile/<pk>',views.profile, name = 'profile'),
+    path('search/',views.search_results, name='search_results'),
+    path('submit-project',views.submit_project, name='submit_project'),
+    path('site_details/<int:id>/',views.site_details,name='site_details'),
 ]
